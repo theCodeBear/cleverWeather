6c43e13df184bdfbdb5e5ee09ab16ac1e089d1a6
Todd Kronenberg <toddkronenberg@gmail.com>
2014-11-07 09:39:27 -0800

    set up getting data for the three times of the day

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function b(){
+$(function() {
   var apiKey = 'a460b122b56d6a8774c447d1b123a4b1';
   var url = 'https://api.forecast.io/forecast/';
   var lati = 37.8267;
@@ -11,7 +11,7 @@ function b(){
   $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
     //console.log(data);
   //  console.log(new Date());
-   $('#weather').html('and the temperature is: ' + data.currently.temperature);
+   $('#weather').html(data.currently.temperature + "<span class='subscript'> f</span>");
   });
 
     var dateObj = new Date();
@@ -19,14 +19,26 @@ function b(){
     var day = dateObj.getDate()+1;
     (day < 10) ? day = "0"+day : day=day;
     var year = dateObj.getFullYear();
-    var time = year+"-"+month+"-"+day+"T08:00:00";
+    var date = year+"-"+month+"-"+day;
 
-$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + time + '?callback=?', function(data) {
-    alert("yo");
+// MORNING
+$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T08:00:00" + '?callback=?', function(data) {
     console.log("8am: " + data.currently.temperature);
+
+});
+
+// AFTERNOON
+$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T13:00:00" + '?callback=?', function(data) {
+    console.log("1pm: " + data.currently.temperature);
+
+});
+
+// EVENING
+$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T18:00:00" + '?callback=?', function(data) {
+    console.log("6pm: " + data.currently.temperature);
+
 });
 
 
 
-
-}
+});
