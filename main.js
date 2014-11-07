63caf5306d8544a2c158af40ac3b5716c0b1d394
Todd Kronenberg <toddkronenberg@gmail.com>
2014-11-07 10:22:13 -0800

    Modified main.js to display time based results according to which carousel page is active

--- main.js
+++ main.js
@@ -3,42 +3,44 @@ $(function() {
   var url = 'https://api.forecast.io/forecast/';
   var lati = 37.8267;
   var longi = -122.423;
-  var data;
-  var morning = { temp: "", weather: ""};
-  var afternoon = { temp: "", weather: ""};
-  var evening = { temp: "", weather: ""};
+  // var data;
+  // var morning = { temp: "", weather: ""};
+  // var afternoon = { temp: "", weather: ""};
+  // var evening = { temp: "", weather: ""};
 
   $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-    //console.log(data);
-  //  console.log(new Date());
-   $('#weather').html(data.currently.temperature + "<span class='subscript'> f</span>");
+   $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
   });
 
     var dateObj = new Date();
-    var month = dateObj.getMonth() + 1; //months from 1-12
-    var day = dateObj.getDate()+1;
-    (day < 10) ? day = "0"+day : day=day;
-    var year = dateObj.getFullYear();
-    var date = year+"-"+month+"-"+day;
+    var day = dateObj.getDate()+1;  // date starts from 0 (as does month)
+    (day < 10) ? day = "0"+day : day=day; // add 0 to front of day because day has to be two digits
+    var date = dateObj.getFullYear()+"-"+ (dateObj.getMonth()+1) + "-" + day;
 
 // MORNING
-$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T08:00:00" + '?callback=?', function(data) {
-    console.log("8am: " + data.currently.temperature);
+if ($('.active').attr('data-slide-to') == 0) {
+  $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T08:00:00" + '?callback=?', function(data) {
+      $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
+      console.log("8am: " + data.currently.temperature);
 
 });
+}
 
 // AFTERNOON
-$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T13:00:00" + '?callback=?', function(data) {
-    console.log("1pm: " + data.currently.temperature);
-
-});
+if ($('.active').attr('data-slide-to') == 1) {
+  $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T13:00:00" + '?callback=?', function(data) {
+    $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
+      console.log("1pm: " + data.currently.temperature);
+  });
+}
 
 // EVENING
-$.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T18:00:00" + '?callback=?', function(data) {
-    console.log("6pm: " + data.currently.temperature);
-
-});
-
+if ($('.active').attr('data-slide-to') == 2) {
+  $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T18:00:00" + '?callback=?', function(data) {
+    $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
+      console.log("6pm: " + data.currently.temperature);
+  });
+}
 
 
 });
