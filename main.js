b22a2b583ff5dbedc310d88231814ad969905eb9
Todd Kronenberg <toddkronenberg@gmail.com>
2014-11-07 11:29:52 -0800

    Got the weather at different times to display

--- main.js
+++ main.js
@@ -8,39 +8,70 @@ $(function() {
   // var afternoon = { temp: "", weather: ""};
   // var evening = { temp: "", weather: ""};
 
-  $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
-   $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
-  });
+  // $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?", function(data) {
+  //  $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
+  // });
 
     var dateObj = new Date();
     var day = dateObj.getDate()+1;  // date starts from 0 (as does month)
     (day < 10) ? day = "0"+day : day=day; // add 0 to front of day because day has to be two digits
     var date = dateObj.getFullYear()+"-"+ (dateObj.getMonth()+1) + "-" + day;
 
-// MORNING
-if ($('.active').attr('data-slide-to') == 0) {
-  $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T08:00:00" + '?callback=?', function(data) {
-      $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
-      console.log("8am: " + data.currently.temperature);
+// var slide = 0;
 
-});
-}
+
+// if ($('a').click(function() {
+// // MORNING
+//   var time = "";
+//   (++slide == 4) ? slide=0 : slide=slide;
+//   if (slide=0)
+//     time = "T08:00:00";
+//   else if (slide=1)
+//     time = "T13:00:00";
+//   else if (slide=2)
+//     time = "T18:00:00";
+
+  $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T08:00:00" + '?callback=?', function(data) {
+      $('#slide0').html('<h1 class="cover-heading temp-num" >' + Math.round(data.currently.temperature) + ' &#xb0<span class="subscript"> f</span></h1>' +
+                    '<i class="fa fa-cloud fa-5x cloudy animated swing"></i>' +
+                    '<p class="lead">Cloudy with a chance of meatballs</p>' +
+                    '<p class="lead">' +
+                    ' You should probably wear<br /><img src="img/t-shirt.png" class="icon">' +
+                    '</p>' +
+                    '8:00am');
+
+      console.log("8am: " + data.currently.temperature);
+  });
+// }));
 
 // AFTERNOON
-if ($('.active').attr('data-slide-to') == 1) {
+// if ($('.active').attr('data-slide') == 1) {
   $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T13:00:00" + '?callback=?', function(data) {
-    $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
+    $('#slide1').html('<h1 class="cover-heading temp-num">' + Math.round(data.currently.temperature) + ' &#xb0<span class="subscript"> f</span></h1>' +
+                  '<i class="fa fa-cloud fa-5x cloudy animated swing"></i>' +
+                    '<p class="lead">Cloudy with a chance of meatballs</p>' +
+                    '<p class="lead">' +
+                    ' You should probably wear<br /><img src="img/t-shirt.png" class="icon">' +
+                    '</p>' +
+                    '1:00pm');
+                    
       console.log("1pm: " + data.currently.temperature);
   });
-}
+// }
 
 // EVENING
-if ($('.active').attr('data-slide-to') == 2) {
+// if ($('.active').attr('data-slide') == 2) {
   $.getJSON(url + apiKey + "/" + lati + "," + longi + "," + date + "T18:00:00" + '?callback=?', function(data) {
-    $('#weather').html(data.currently.temperature + "&#xb0<span class='subscript'> f</span>");
+    $('#slide2').html('<h1 class="cover-heading temp-num">' + Math.round(data.currently.temperature) + ' &#xb0<span class="subscript"> f</span></h1>' +
+                    '<i class="fa fa-cloud fa-5x cloudy animated swing"></i>' +
+                    '<p class="lead">Cloudy with a chance of meatballs</p>' +
+                    '<p class="lead">' +
+                    ' You should probably wear<br /><img src="img/t-shirt.png" class="icon">' +
+                    '</p>' +
+                    '6:00pm');
       console.log("6pm: " + data.currently.temperature);
   });
-}
+// }
 
 
 });
